45006da7d71cb5a5c6efdb33f18d31599608b0e5
jay shah <jay@bayesconsulting.io>
2015-02-23 18:27:31 -0600

    text message formatting

--- misc/util.py
+++ misc/util.py
@@ -1,7 +1,7 @@
 """Misc functions needed for text_dj"""
 
 def stringify_results(results):
-	text_response = "Text back the number of your preffered song or search again for another song: \n"
+	text_response = "Text back the number of your preffered song or search again for another song: \n\n"
 
 	for i in range(0,len(results)):
 		text_response = text_response + str(i) + ": " + results[i]['name'] + ", by " + results[i]['artists'] + "\n\n"
