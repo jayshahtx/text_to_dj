19f86023a64ff1678cee7cd12852d2c8b42921f7
jay shah <jay@bayesconsulting.io>
2015-02-23 18:23:55 -0600

    syntax error

--- misc/util.py
+++ misc/util.py
@@ -4,5 +4,5 @@ def stringify_results(results):
 	text_response = "Text back the number of your preffered song or search again for another song: \n"
 
 	for i in range(0,len(results)):
-		text_response = text_response + str(i) + ": " + results[i]['name'] + " " + results[i]['artists'] "\n"
+		text_response = text_response + str(i) + ": " + results[i]['name'] + " " + results[i]['artists'] + "\n"
 	return text_response 
